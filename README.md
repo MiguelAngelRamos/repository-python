19e1c369e6583d6cdd4f787ea8e9519bb36c2ea1
Miguel Ramos <return.infox@gmail.com>
2024-02-28 21:46:12 -0300

    codigo de libreria

--- README.md
+++ README.md
@@ -5,3 +5,22 @@
 3. Aprender a ejecutar el metodo principal "main"
 4. Modularizar la app utilizando el archivo __init__
 5. Aprender a integrar librerias propias de python como la libreria math.
+
+
+```python 
+
+  is_active = True
+  authorization = False
+
+  is_active and authorization -> True
+
+  # Vas al cine
+  boy_year = int(input("Ingrese la edad: ")) # 18
+  cash = False
+  if boy_year >= 18 and cash:
+      print("puede ir a ver la pelicula")
+  else
+      print("No puedes ir a ver la peli")
+
+
+```
---
+++ ejercicios/05-ejercicio.py
@@ -0,0 +1,57 @@
+# Iniciar el Stock de Libro
+# Simular los prestamos
+# Gestionar Devoluciones
+# Notificar disponibilidad
+# Limitación
+# Reporte de Stock
+
+# librerias de python a utilizar "time", "random"
+
+import time
+import random
+
+# Inicializacion de los stocks de los libros
+stock_mascara_roja = 2
+stock_billy_summers = 2
+
+# Contador de operaciones
+contador_operaciones = 0
+
+# Simulacion de prestamos
+def simular_prestamo():
+    global stock_mascara_roja, stock_billy_summers, contador_operaciones
+
+    # Selección aleatoria de libro y cantidad de prestamo
+    libro = random.choice([1, 2])
+    cantidad = random.randint(1,3) # cantidad de prestamo
+
+    # Procesamiento de préstamo
+    if libro == 1 and stock_mascara_roja >= cantidad:
+        stock_mascara_roja = stock_mascara_roja - cantidad
+        # stock_mascara_roja -= cantidad
+        print(f"Prestamo de {cantidad} unidad(es) del libro mascara roja realizado")
+    elif libro == 2 and stock_billy_summers >= cantidad:
+        stock_billy_summers = stock_billy_summers - cantidad
+        print(f"Prestamo de {cantidad} unidad(es) del libro billy summers realizado")
+    else:
+        print("No hay stock suficiente para el préstamos solicitado")
+
+    contador_operaciones = contador_operaciones + 1;
+    verificar_notificaciones()
+
+def verificar_notificaciones():
+    # Verificar disponibilidad para notificar a los usuarios
+    if stock_mascara_roja > 0:
+        print("Notificacion: el Libro mascara roja, si esta disponible para prestamo.")
+    if stock_billy_summers > 0:
+        print("Notificacion: el libro billy summers, si esta disponible para prestamo")
+
+    # Reporte de stock cada 10 operaciones
+    if contador_operaciones % 10 == 0:
+        print(f"Reporte de stock - Libro Mascara Roja: {stock_mascara_roja}, Libro Billy Summers: {stock_billy_summers}")
+
+
+# Simulacion de una bibioleteca
+for _ in range(5):
+    simular_prestamo()
+    time.sleep(5)
