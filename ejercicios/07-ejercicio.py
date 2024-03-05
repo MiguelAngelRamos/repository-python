527d89795d040b8dc6c4d1b40d2576d3198c0871
Miguel Ramos <return.infox@gmail.com>
2024-03-05 20:36:25 -0300

    solucion de logica de caracteres part 1

--- ejercicios/07-ejercicio.py
+++ ejercicios/07-ejercicio.py
@@ -5,3 +5,29 @@
 # 1. Una palabra se define como una secuencia de caracteres alfabéticos (a-z, A-Z), y las palabras son insensibles a las mayúsculas y minúsculas (es decir, "Hola" y "hola" se consideran la misma palabra).
 # 2. Ignora cualquier carácter que no sea alfabético, considerándolos como separadores de palabras.
 # 3. Asume que el texto no contiene caracteres especiales de otros idiomas (acentos, eñes, etc.), pero tu solución debe ser fácilmente adaptable para incluirlos.
+
+def counter_word(text):
+    text = text.lower()
+
+    clean_text = ""
+
+    for caracter in text:#hola 2024
+        if caracter.isalpha():
+            #* "" = "" + "h"
+            #* "h"
+            #* h = h + o
+            #* ho
+            # clean_text += caracter
+            clean_text = clean_text + caracter
+        else:
+            clean_text = clean_text + " "
+
+    
+    words = clean_text.split()
+    print(words)
+
+    
+text = "Hola, ¿cómo estás? !Yo estoy genial, apriendo ejercicios de lógica en python 3."
+counter_word(text)
+    
+          
